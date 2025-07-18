5fdcc23efad780dfbc9882b1bac5c86914f3870f
GlobalSaturation <87565097+GlobalSaturation@users.noreply.github.com>
2025-07-18 15:33:33 -0700

    Add display and remove unused code
    Functions like capitalize() and playGame() are no
    longer needed because the game has moved away from
    a console-based interface.

--- javascript.js
+++ javascript.js
@@ -4,37 +4,12 @@ function getComputerChoice() {
 
 	switch(randomNum) {
 		case 1:
-			return "rock";
+			return "Rock";
 		case 2:
-			return "paper";
+			return "Paper";
 		case 3:
-			return "scissors";
+			return "Scissors";
 	}
-}
-
-function validateAnswer(answer) {
-	//smooth out any strange case before checking
-	answer = answer.toLowerCase();
-
-	if (answer == "rock" || answer == "paper" || answer == "scissors")
-		return true;
-	else
-		return false;
-}
-
-function getHumanChoice() {
-	let answer = window.prompt("Rock, paper, or scissors?");
-
-	while(!validateAnswer(answer)) {
-		answer = window.prompt("Invalid answer. Rock, paper, or scissors?");
-	}
-	return answer;
-}
-
-function capitalize(word) {
-	word = word.toLowerCase();
-	let firstChar = word.at(0);
-	return word.replace(firstChar, firstChar.toUpperCase());
 }
 
 function didIWin(humanChoice, computerChoice) {
@@ -47,11 +22,11 @@ function didIWin(humanChoice, computerChoice) {
 	else if (humanChoice == "Scissors" && computerChoice == "paper")
 		isWinner = true;
 
-	if (computerChoice == "rock" && humanChoice== "scissors")
+	if (computerChoice == "Rock" && humanChoice== "Scissors")
 		isWinner = false;
-	else if (computerChoice == "paper" && humanChoice == "rock")
+	else if (computerChoice == "Paper" && humanChoice == "Rock")
 		isWinner = false;
-	else if (computerChoice == "scissors" && humanChoice == "paper")
+	else if (computerChoice == "Scissors" && humanChoice == "Paper")
 		isWinner = false;
 
 	return isWinner;
@@ -61,66 +36,47 @@ function didIWin(humanChoice, computerChoice) {
 function playRound(humanChoice, computerChoice) {
 	let winner = "nobody";
 
-	humanChoice = capitalize(humanChoice);
-	computerChoice = capitalize(computerChoice);
-	
-	console.log(`You chose ${humanChoice} and the CPU chose ${computerChoice}`);
+	display(`You chose ${humanChoice} and the CPU chose ${computerChoice}`);
 
 	if (humanChoice === computerChoice) {
-		console.log(`Tie! Both sides picked ${humanChoice}`);
+		display(`Tie! Both sides picked ${humanChoice}`);
 	}
 	else if (didIWin(humanChoice, computerChoice)) {
-		console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+		display(`You win! ${humanChoice} beats ${computerChoice}`);
 		winner = "human";
 	}
 	else {
-		console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+		display(`You lose! ${computerChoice} beats ${humanChoice}`);
 		winner = "computer";
 	}
 	
 	return winner;
 }
 
-function playGame() {
-	let humanScore = 0;
-	let computerScore = 0;
-
-	//play 5 rounds
-	for (let roundNum = 1; roundNum <= 5; roundNum++) {
-		console.log("Round #" + roundNum);
-
-		let humanChoice = getHumanChoice();
-		let computerChoice = getComputerChoice();
-		let winner = playRound(humanChoice, computerChoice);
-
-		//update score based on winner of the round
-		if (winner == "human")
-			humanScore++;
-		else if (winner == "computer")
-			computerScore++;
-
-		console.log(`Score: ${humanScore}-${computerScore}`);
-	}
-
-	//Determine overall winner
-	if (humanScore > computerScore) {
-		console.log("You won the game!");
-	}
-	else if (humanScore < computerScore) {
-		console.log("You lost the game. Better luck next time!");
-	}
-	else {
-		console.log("The game tied!");
-	}
+function display(message) {
+	const div = document.querySelector("#display");
+	div.innerText = message;
 }
 
+//make all buttons clickable
 const buttons = document.querySelectorAll("button");
 buttons.forEach((btn) => {
-	//set up event listeners for each button that give thier respective answer
 	btn.addEventListener("click", () => {
-		playRound(btn.innerText, getComputerChoice());
+		const winner = playRound(btn.innerText, getComputerChoice());
+		updateScore(winner);
 	});
 })
 
-//start the game
-//playGame();
+//global score variables
+let humanScore = 0;
+let computerScore = 0;
+
+function updateScore(winner) {
+	if (winner == "human")
+		humanScore++;
+	else if (winner == "computer")
+		computerScore++;
+
+	const scoreBoard = document.querySelector("#score");
+	scoreBoard.innerText = `Score: ${humanScore}-${computerScore}`;
+}
